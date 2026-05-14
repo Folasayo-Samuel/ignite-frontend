578f605365c5ead5f9063f5efaabb4437b197266
Folasayo Samuel Olayemi <folasayooluwadamilare@gmail.com>
2026-05-14 13:22:00 +0100

    Implemented some features

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.paystack.co; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https://res.cloudinary.com https://avatars.githubusercontent.com https://ui-avatars.com; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://fola-ignite-backend.onrender.com http://localhost:4000 http://localhost:5000 wss://fola-ignite-backend.onrender.com ws://localhost:4000 ws://localhost:5000 https://api.paystack.co; frame-src 'self' https://js.paystack.co;"
+  }
+];
+
 const nextConfig = {
   // eslint: {
   //   ignoreDuringBuilds: true,
@@ -21,6 +53,14 @@ const nextConfig = {
         source: "/student/:path*",
         destination: "/learner/:path*",
         permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
       },
     ];
   },
